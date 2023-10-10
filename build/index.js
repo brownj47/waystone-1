194d1da0783779090615bc4bc1a0799c714c6eaf
Justus Brown <justus.c.brown@gmail.com>
2023-10-10 16:32:02 -0700

    landing page basic layout

--- build/index.js
+++ build/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log('Hello World');
+class Character {
+    constructor(name, characterClass = 'Fighter', level = 1, strength = 10, dexterity = 10, constitution = 10, intelligence = 10, wisdom = 10, charisma = 10) {
+        this.name = name;
+        this.characterClass = characterClass;
+        this.level = level;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.charisma = charisma;
+        this.name = name;
+        this.characterClass = characterClass;
+        this.level = level;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.constitution = constitution;
+        this.intelligence = intelligence;
+        this.wisdom = wisdom;
+        this.charisma = charisma;
+    }
+}
+const characterCreationForm = document.querySelector('#character-creation-form');
+const playerCharacter = new Character('FNU LNU');
+characterCreationForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const characterNameInput = event.target[0];
+    playerCharacter.name = characterNameInput.value;
+    console.log(playerCharacter);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello World')
+class Character {
+    constructor(
+        public name: string,
+        public characterClass: string = 'Fighter',
+        public level: number = 1,
+        public strength: number = 10,
+        public dexterity: number = 10,
+        public constitution: number = 10,
+        public intelligence: number = 10,
+        public wisdom: number = 10,
+        public charisma: number = 10,
+    ) {
+        this.name = name
+        this.characterClass = characterClass
+        this.level = level
+        this.strength = strength
+        this.dexterity = dexterity
+        this.constitution = constitution
+        this.intelligence = intelligence
+        this.wisdom = wisdom
+        this.charisma = charisma
+    }
+
+}
+
+const characterCreationForm = document.querySelector('#character-creation-form') as HTMLFormElement
+
+const playerCharacter = new Character('FNU LNU')
+
+characterCreationForm.addEventListener('submit', (event: SubmitEvent) => {
+    event.preventDefault()
+    const characterNameInput = (event.target as HTMLFormElement)[0] as HTMLInputElement
+    playerCharacter.name = characterNameInput.value
+    console.log(playerCharacter)
+})
+
