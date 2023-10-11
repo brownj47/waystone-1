0d2687ce8db29a0cf0b6453d2fa3ea267b71ed68
Justus Brown <justus.c.brown@gmail.com>
2023-10-10 20:36:18 -0700

    get stat selection working

--- build/index.js
+++ build/index.js
@@ -1,31 +1,69 @@
 "use strict";
+const characterCreationForm = document.querySelector('#character-creation-form');
+const characterNameInput = document.querySelector('#character-name-input');
+const availablePointsCounter = document.querySelector('#available-points-counter');
 class Character {
-    constructor(name, characterClass = 'Fighter', level = 1, strength = 10, dexterity = 10, constitution = 10, intelligence = 10, wisdom = 10, charisma = 10) {
+    constructor(name, background = 'none', strength = 1, dexterity = 1, constitution = 1, availablePoints = 1) {
         this.name = name;
-        this.characterClass = characterClass;
-        this.level = level;
+        this.background = background;
         this.strength = strength;
         this.dexterity = dexterity;
         this.constitution = constitution;
-        this.intelligence = intelligence;
-        this.wisdom = wisdom;
-        this.charisma = charisma;
+        this.availablePoints = availablePoints;
         this.name = name;
-        this.characterClass = characterClass;
-        this.level = level;
+        this.background = background;
         this.strength = strength;
         this.dexterity = dexterity;
         this.constitution = constitution;
-        this.intelligence = intelligence;
-        this.wisdom = wisdom;
-        this.charisma = charisma;
+    }
+    incrementStat(statName) {
+        if (this.availablePoints > 0) {
+            console.log("Incrementing: ", statName, this[statName], "+ 1");
+            this[statName] += 1;
+            this.availablePoints -= 1;
+            this._updateStatDisplay(statName, this[statName]);
+        }
+        else {
+            console.log(`Cannot increment ${statName}, no points available.`);
+        }
+    }
+    decrementStat(statName) {
+        if (this[statName] >= 1) {
+            console.log("Decrementing: ", statName, this[statName], "- 1");
+            this[statName] -= 1;
+            this.availablePoints += 1;
+            this._updateStatDisplay(statName, this[statName]);
+        }
+        else {
+            console.log(`Cannot decrement ${statName} below 1`);
+        }
+    }
+    _updateStatDisplay(statName, statValue) {
+        console.log(`Updating ${statName} display element to ${statValue}`);
+        availablePointsCounter.textContent = this.availablePoints.toString();
+        const statCounter = document.querySelector(`#${statName}-counter`);
+        statCounter.textContent = statValue.toString();
     }
 }
-const characterCreationForm = document.querySelector('#character-creation-form');
 const playerCharacter = new Character('FNU LNU');
 characterCreationForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const characterNameInput = event.target[0];
     playerCharacter.name = characterNameInput.value;
-    console.log(playerCharacter);
+    console.log("CREATED: ", playerCharacter);
 });
+function hydrateStatRadialInputs(statName) {
+    const incrementBtn = document.querySelector(`#${statName}-increment`);
+    const decrementBtn = document.querySelector(`#${statName}-decrement`);
+    incrementBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        playerCharacter.incrementStat(statName);
+    });
+    decrementBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        playerCharacter.decrementStat(statName);
+    });
+}
+hydrateStatRadialInputs("strength");
+hydrateStatRadialInputs("dexterity");
+hydrateStatRadialInputs("constitution");
+// when the user types, if the number is between 1 and 5, update the character's strength
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,81 @@
+const characterCreationForm = document.querySelector('#character-creation-form') as HTMLFormElement
+const characterNameInput = document.querySelector('#character-name-input') as HTMLInputElement
+const availablePointsCounter = document.querySelector('#available-points-counter') as Element
+
 class Character {
     constructor(
         public name: string,
-        public characterClass: string = 'Fighter',
-        public level: number = 1,
-        public strength: number = 10,
-        public dexterity: number = 10,
-        public constitution: number = 10,
-        public intelligence: number = 10,
-        public wisdom: number = 10,
-        public charisma: number = 10,
+        public background: string = 'none',
+        public strength: number = 1,
+        public dexterity: number = 1,
+        public constitution: number = 1,
+        public availablePoints: number = 1
     ) {
         this.name = name
-        this.characterClass = characterClass
-        this.level = level
+        this.background = background
         this.strength = strength
         this.dexterity = dexterity
         this.constitution = constitution
-        this.intelligence = intelligence
-        this.wisdom = wisdom
-        this.charisma = charisma
+
     }
 
-}
+    incrementStat(statName: string): void {
+        if (this.availablePoints > 0) {
+            console.log("Incrementing: ", statName, (this as any)[statName], "+ 1");
+            (this as any)[statName] += 1
+            this.availablePoints -= 1
+            this._updateStatDisplay(statName, (this as any)[statName])
+        } else {
+            console.log(`Cannot increment ${statName}, no points available.`)
+        }
+    }
 
-const characterCreationForm = document.querySelector('#character-creation-form') as HTMLFormElement
+    decrementStat(statName: string): void {
+        if ((this as any)[statName] >= 1) {
+            console.log("Decrementing: ", statName, (this as any)[statName], "- 1");
+            (this as any)[statName] -= 1
+            this.availablePoints += 1
+            this._updateStatDisplay(statName, (this as any)[statName])
+        } else {
+            console.log(`Cannot decrement ${statName} below 1`)
+        }
+    }
+
+    _updateStatDisplay(statName: string, statValue: number): void {
+        console.log(`Updating ${statName} display element to ${statValue}`)
+        availablePointsCounter.textContent = this.availablePoints.toString()
+        const statCounter = document.querySelector(`#${statName}-counter`) as Element
+        statCounter.textContent = statValue.toString()
+    }
+}
 
 const playerCharacter = new Character('FNU LNU')
 
 characterCreationForm.addEventListener('submit', (event: SubmitEvent) => {
     event.preventDefault()
-    const characterNameInput = (event.target as HTMLFormElement)[0] as HTMLInputElement
     playerCharacter.name = characterNameInput.value
-    console.log(playerCharacter)
+    console.log("CREATED: ", playerCharacter)
 })
 
+function hydrateStatRadialInputs(statName: string) {
+    const incrementBtn = document.querySelector(`#${statName}-increment`) as Element
+    const decrementBtn = document.querySelector(`#${statName}-decrement`) as Element
+
+    incrementBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        playerCharacter.incrementStat(statName)
+    })
+
+    decrementBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        playerCharacter.decrementStat(statName)
+    })
+}
+
+hydrateStatRadialInputs("strength")
+hydrateStatRadialInputs("dexterity")
+hydrateStatRadialInputs("constitution")
+
+
+
+// when the user types, if the number is between 1 and 5, update the character's strength
